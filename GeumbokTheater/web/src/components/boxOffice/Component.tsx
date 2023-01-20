5c40e785471baaddc997214df54a0ded57d11b4c
예나나 <111760323+yesla-yeom@users.noreply.github.com>
2023-01-20 16:59:49 +0900

    added useNavigate

--- GeumbokTheater/web/src/components/boxOffice/Component.tsx
+++ GeumbokTheater/web/src/components/boxOffice/Component.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 import img1 from "./poster.jpeg";
+import { useNavigate } from "react-router-dom";
 
 export default function Component() {
+  const navigate = useNavigate();
+  const goMovieInfo = () => {
+    navigate("/movieinfo");
+  };
+
   return (
     <BoxOfBox>
       <BoxFrame>
@@ -10,7 +16,7 @@ export default function Component() {
       <TitleDetail>10개의 영화가 검색되었습니다.</TitleDetail>
       <div className="bigOne">
         <div className="one">
-          <img src={img1}></img>
+          <img src={img1} onClick={goMovieInfo}></img>
           <div className="movie_title">미니언즈</div>
           <div className="movie_detail">
             <div>누적관람수 303,030명</div>
--- GeumbokTheater/web/src/components/movieInfo/Component.tsx
+++ GeumbokTheater/web/src/components/movieInfo/Component.tsx
@@ -1,1 +1,36 @@
-import React from "react";
+import styled from "styled-components";
+import posterImg from "./poster2.jpg";
+
+export default function Component() {
+  return (
+    <BgImgBox>
+      <BgImgFrame>
+        <div>ㅎㅇㅎㅇ</div>
+      </BgImgFrame>
+    </BgImgBox>
+  );
+}
+
+const BgImgBox = styled.div`
+  width: 100vw;
+  background-color: black;
+  position: relative;
+  height: 520px;
+  margin: 0 0 40px 0;
+`;
+
+const BgImgFrame = styled.div`
+  width: 60%;
+  position: absolute;
+  left: 50%;
+  top: 0;
+  background-image: url(${posterImg});
+  background-repeat: no-repeat;
+  background-size: 100% auto;
+  z-index: 1;
+  opacity: 0.8;
+
+  div {
+    color: white;
+  }
+`;
