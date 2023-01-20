9bdda04568af925cb6e959f04125883c226d90cb
예나나 <111760323+yesla-yeom@users.noreply.github.com>
2023-01-20 16:59:49 +0900

    added useNavigate

--- GeumbokTheater/web/src/components/movieInfo/Container.tsx
+++ GeumbokTheater/web/src/components/movieInfo/Container.tsx
@@ -1,1 +1,6 @@
-import React from "react";
+import Component from "../boxOffice/Component";
+import { useNavigate } from "react-router-dom";
+
+export default function Container() {
+  return <Component></Component>;
+}
