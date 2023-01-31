aa54646b8c8e5af7c8b5b73257bd7807d8c99415
Nyes98 <111405218+Nyes98@users.noreply.github.com>
2023-01-31 20:57:14 +0900

    footer

---
+++ GeumbokTheater/web/src/components/Footer/Containers.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import MainFooterComponent from "./Component";
+
+const MainFooterContainer = () => {
+  return <MainFooterComponent></MainFooterComponent>;
+};
+export default MainFooterContainer;
